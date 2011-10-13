72331ae8d272ece00083c3d45027ada05e981e77
Gorka Maiztegi <gmaiztegi@gmail.com>
2011-10-13 18:42:12 +0200

    Cambiado comentarios a llave  {* *}

--- src/compilador/Patterns.java
+++ src/compilador/Patterns.java
@@ -16,7 +16,7 @@ public class Patterns {
     
     public static final Pattern INTEGER = Pattern.compile("[+-]?[0-9]+");
     public static final Pattern FLOAT = Pattern.compile("[+-]?[0-9]+\\.[0-9]+([eE][+-]?[0-9]+)?");
-    public static final Pattern COMMENT = Pattern.compile("\\(\\*([^*]|(\\*+[^*)]))*\\*+\\)", Pattern.MULTILINE);
+    public static final Pattern COMMENT = Pattern.compile("\\{\\*([^*]|(\\*+[^*}]))*\\*+\\}", Pattern.MULTILINE);
     public static final Pattern IDENT = Pattern.compile("[a-zA-Z](_?[a-zA-Z0-9])*");
     public static final Pattern SPECIALDOUBLE = Pattern.compile("(:=|<=|>=)");
     public static final Pattern SPECIALCHAR = Pattern.compile("[-+*/()\\\\:,;$%&|~{}<>\\[\\]=^]");
