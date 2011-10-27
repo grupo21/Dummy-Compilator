cd8f719fb50a7777d3dabe5ce05e62c3750b7a50
Gorka Maiztegi <gmaiztegi@gmail.com>
2011-10-27 18:22:23 +0200

    Añadida Excepción UnexpectedTokenException

---
+++ src/compilador/UnexpectedTokenException.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author gmaiztegi
+ */
+public class UnexpectedTokenException extends RuntimeException {
+}
