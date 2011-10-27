fbe8bc47546c95f05391ad0c48def1681f8d9097
Gorka Maiztegi <gmaiztegi@gmail.com>
2011-10-27 19:20:16 +0200

    Creada clase Abstracta para ETDSs

---
+++ src/compilador/ETDS/AbstractETDS.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador.ETDS;
+
+import compilador.*;
+
+/**
+ * Token representando un flotante
+ * @author Jon Aguirre <jaguirre026@ehu.es>
+ * @author Ander Arbelaitz <aarbelaiz004@ehu.es>
+ * @author Gorka Maiztegi <gmaiztegi001@ehu.es>
+ */
+public abstract class AbstractETDS {
+    
+    public abstract void execute(AbstractETDS parent);
+    
+    public Token expectType(int type) {
+        Token token;
+        
+        token = Tokenizer.tokenizer.nextElement();
+        
+        if (!token.isType(type)) {
+            throw new UnexpectedTokenException();
+        }
+        
+        return token;
+    }
+    
+    public Token expectString(String str) {
+        Token token;
+        
+        token = Tokenizer.tokenizer.nextElement();
+        
+        if (!token.getMatch().equals(str)) {
+            throw new UnexpectedTokenException();
+        }
+        
+        return token;
+    }
+}
