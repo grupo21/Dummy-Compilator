e63ab42a8e149152ed8f871e227daf9868e8bac4
Gorka Maiztegi <gmaiztegi@gmail.com>
2011-10-27 19:20:30 +0200

    Añadidos dos ETDSs vacíos

---
+++ src/compilador/ETDS/Declarations.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador.ETDS;
+
+/**
+ *
+ * @author gmaiztegi
+ */
+public class Declarations {
+    
+}
