3fb272028c3ff5b1a974c1d27c1aebbdcf0699c2
Gorka Maiztegi <gmaiztegi@gmail.com>
2011-11-08 16:21:48 +0100

    Añadidos tipos de símbolos adicionales

--- src/compilator/Symbol/Symbol.java
+++ src/compilator/Symbol/Symbol.java
@@ -11,6 +11,9 @@ package compilator.Symbol;
 public class Symbol {
     public static final int INTEGER = 0;
     public static final int FLOAT = 1;
+    public static final int PROCEDURE = 2;
+    public static final int FUNCTION = 3;
+    public static final int PROGRAM = 4;
     
     protected String name;
     protected int type;
